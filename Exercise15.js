2234794a841db26030363ffa55aa47ddbc6ae29a
rakdhan <58630970+rakdhan@users.noreply.github.com>
2020-01-15 17:39:33 +0700

    revisi 15 Jan

--- Exercise15.js
+++ Exercise15.js
@@ -1,39 +1,28 @@
 function groupAnimals(animals) {
-    // you can only write your code here!
-    var awalA = []
-    var awalC = []
-    var awalK = []
-    var awalU = []
-    var result =[]
+    var result = []
     for (let i = 0; i < animals.length; i++) {
-        // console.log(animals[i][0])
-        if (animals[i][0] === 'a') {
-            awalA.push(animals[i])
-            // console.log(awalA)
+        var cekBoolean = false
+        for (let j = 0; j < result.length; j++) {
+            // console.log(animals[i][0], result[j][0][0])
+            if (animals[i][0] === result[j][0][0]) {
+                // console.log(result[j][0][0])
+                cekBoolean = true
+                result[j].push(animals[i])
+            }
+            
         }
-        else if (animals[i][0] === 'c') {
-            awalC.push(animals[i])
+        if (!cekBoolean) {
+            result.push([animals[i]])
         }
-        else if (animals[i][0] === 'k') {
-            awalK.push(animals[i])
-        }
+        
     }
-    result.push(awalA, awalC, awalK)
-
-    for (let i = 0; i < animals.length; i++) {
-        if (animals[i][0] === 'u') {
-            awalU += animals[i]
-            result.push([awalU])
-        }
-    }    
     return result
-  }
+}
   
-  // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
-
+// TEST CASES
+// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
 
  
